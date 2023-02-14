67edcb04400e7d6950ca6b8cf887136d4479c476
Matii Renk <renkmatias@gmail.com>
2023-02-14 15:40:22 -0300

    basepage imp and contactandroidmodel imp

--- src/main/java/android/pages/BasePage.java
+++ src/main/java/android/pages/BasePage.java
@@ -1,5 +1,14 @@
 package android.pages;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import utils.DriverSetup;
 
+public class BasePage extends DriverSetup {
+    public void tapOn(By mobileElement) {
+        getDriver().findElement(mobileElement).click();
+    }
+
+    public void writeIn(By mobileElement, String textToInput) {
+        getDriver().findElement(mobileElement).sendKeys(textToInput);
+    }
 }
