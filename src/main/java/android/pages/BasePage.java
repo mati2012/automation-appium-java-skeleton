24906c98d6dbea874952c0dfc8c7222fa28c8431
Matii Renk <renkmatias@gmail.com>
2023-02-14 14:46:26 -0300

    initial commit + driversetup class

---
+++ src/main/java/android/pages/BasePage.java
@@ -0,0 +1,5 @@
+package android.pages;
+
+public class BasePage {
+
+}
---
+++ src/main/java/utils/DriverSetup.java
@@ -0,0 +1,67 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DriverSetup {
+    private static final Dotenv dotEnv = Dotenv.load();
+    private static AppiumDriverLocalService service;
+    private static AppiumDriver driver;
+
+    @BeforeClass
+    public static void startServer() {
+        //starts appium service
+        service = new AppiumServiceBuilder()
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @BeforeClass
+    public static void setupDriver() {
+        //set up driver depeding on the platform set on .env
+        String platform = dotEnv.get("PLATFORM");
+        if (platform == null) {
+            throw new RuntimeException("Platform is not defined");
+        }
+        switch (platform.toUpperCase()) {
+            case "IOS" -> driver = iosDriver();
+            case "ANDROID" -> driver = androidDriver();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (driver != null) driver.quit();
+        if (service != null) service.stop();
+    }
+
+    private static AppiumDriver androidDriver() {
+        // setting capabilities for android driver
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, dotEnv.get("PLATFORM"));
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, dotEnv.get("PLATFORM_VERSION"));
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, dotEnv.get("ANDROID_DRIVER"));
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, dotEnv.get("DEVICE_NAME"));
+//     ToDo investigate more about this
+//      desiredCapabilities.setCapability("appPackage", "com.android.contacts");
+//      desiredCapabilities.setCapability("appActivity", ".activities.PeopleActivity");
+        return new AndroidDriver(service.getUrl(), capabilities);
+    }
+
+    private static AppiumDriver iosDriver() {
+        return null;
+    }
+
+    public static AppiumDriver getDriver() {
+        return driver;
+    }
+
+}
